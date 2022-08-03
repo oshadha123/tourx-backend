166b3a4e385cfbe56be7acb3dd9a09a77e90290c
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2022-08-03 11:57:41 +0530

    creating signup file structure

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRouter = require("./api/users/user.router");
+const userRegister = require("./api/register/register.router");
 
 app.use(express.json());
 
-app.use("/api/users", userRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/register", userRegister);
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log("server up and running on PORT :", port);
