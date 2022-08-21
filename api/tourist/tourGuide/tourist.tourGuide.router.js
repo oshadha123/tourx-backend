5ff45106885457672ec81650c9e3dba2df559209
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2022-08-21 12:31:45 +0530

    temp

--- api/tourist/tourGuide/tourist.tourGuide.router.js
+++ api/tourist/tourGuide/tourist.tourGuide.router.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
-// const { getTourGuide } = require("../../../auth/token_validation");
+const { checkTourist } = require("../../../auth/token_validation");
 const {getTourGuide} = require("./tourist.tourGuide.controller");
 
 
 // //router.post("/", register);
 // router.get("/tour",checkTourist, getTours);
-router.get("/tourguide", getTourGuide);
+router.get("/tourguide",checkTourist, getTourGuide);
 
 module.exports = router;
