24004c8b365c709631d31330cd5f919db5c464d8
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2022-08-28 19:18:32 +0530

    admin - user route file structure

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const forgotRouter = require("./api/forgotPassword/forgot.router");
 const getLeaderBoard = require("./api/tourguide/tourguide.home.router")
 const getYourTours = require('./api/tourguide/tourguide.home.router')
 const touristRoutes = require('./api/tourist/tourist.router')
+const adminRoutes = require('./api/admin/admin.router')
 
 app.use(cors())
 app.use(express.json());
@@ -19,7 +20,7 @@ app.use("/api/v1",getLeaderBoard);
 app.use("/api/v1",getYourTours);
 app.use("/api/v1",forgotRouter);
 app.use("/api/v1", touristRoutes);
-
+app.use("/api/v1", adminRoutes);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
