d95bd203d28e6e63548345511492dfcaf1946282
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2022-10-26 16:59:17 +0530

    updated admin stats

--- api/admin/stats/admin.stats.router.js
+++ api/admin/stats/admin.stats.router.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 
 const { checkAdmin } = require("../../../auth/token_validation");
-const {getUserStats,getPaymentStats,getCount,getAccountTypeStats,getNewReportedAccountCount} = require("./admin.stats.controller");
+const {getUserStats,getPaymentStats,getCount,getAccountTypeStats,getNewReportedAccountCount,getTourCount} = require("./admin.stats.controller");
 
 router.get("/stats/userRegistration",checkAdmin, getUserStats);
 router.get("/stats/payment",checkAdmin, getPaymentStats);
 router.get("/stats/user/count", getCount);
 router.get("/stats/user/accountType", getAccountTypeStats);
 router.get("/stats/user/reported", getNewReportedAccountCount);
+router.get("/stats/tour", getTourCount);
 module.exports = router;
